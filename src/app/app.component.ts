7935d114345f85a2513a73c9cfbaa853edffc8f0
GiovanniHM <giovannihernandezm26@gmail.com>
2021-10-26 16:43:53 -0500

    *ngFor para arrays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,40 @@ export class AppComponent {
   arrayEmos: string[] =[ '😂' , '🐦', '🐳','🌮', '💚']
   newEmoji: string;
 
+  products = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
   btnStatus = false;
   progressValor = 33;
   edad = 1;
